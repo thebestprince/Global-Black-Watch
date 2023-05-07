92e4d99793c2c2d9a9fb6d9ed57c258fdb497dbe
Dwayne Reed <dreedjr1992@gmail.com>
2023-05-07 16:44:47 -0500

    footer

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ let hh = day.getHours () * 30;
 let mm = day.getMinutes() * deg;
 let ss = day.getSeconds() * deg;
 
-// hr.style.transform = `rotate(${(hh)+(mm/12)}deg)`;
-//  mn.style.transform = `rotate(${mm}deg)`;
-//  sc.style.transform = `rotate(${ss}deg)`;
+  hr.style.transform = `rotate(${(hh)+(mm/12)}deg)`;
+  mn.style.transform = `rotate(${mm}deg)`;
+  sc.style.transform = `rotate(${ss}deg)`;
 
 
 
